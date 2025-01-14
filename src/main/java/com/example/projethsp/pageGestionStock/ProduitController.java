5c2b66afc7089ff864d7aa1667df1b6d9daa632f
lauPB07 <laurapiresberclaz@gmail.com>
2025-01-14 14:56:16 +0100

    feat : ajout de la fonctionalité de modification d'une fiche produit

--- src/main/java/com/example/projethsp/pageGestionStock/ProduitController.java
+++ src/main/java/com/example/projethsp/pageGestionStock/ProduitController.java
@@ -7,19 +7,20 @@ import com.example.projethsp.Repository.ProduitRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ProduitController implements Initializable {
 
     @FXML
-    private Button modifierButton;
+    private Label labelErreur;
 
     @FXML
     private TableView<FicheProduit> produitTable;
@@ -49,7 +50,6 @@ public class ProduitController implements Initializable {
         ArrayList<FicheProduit> list = produitRepository.recupererProduit();
         produitTable.getItems().addAll(list);
 
-        modifierButton.setVisible(false);
         supprimerButton.setVisible(false);
     }
 
@@ -61,5 +61,41 @@ public class ProduitController implements Initializable {
     @FXML
     public void ajoutProduit(ActionEvent event){
         HelloApplication.changeScene("pageGestionStock/ajoutProduitView", "Ajout de produit");
+    }
+
+
+    @FXML
+    void onListeSelection(MouseEvent event){
+        if(event.getButton() == MouseButton.PRIMARY && event.getClickCount()==2){
+            TablePosition cell = produitTable.getSelectionModel().getSelectedCells().get(0);
+            int indexLigne = cell.getRow();
+            TableColumn colone = cell.getTableColumn();
+            FicheProduit produitsel = produitTable.getItems().get(indexLigne);
+            System.out.println("Double-clique ligne "+indexLigne+" , colone  "+colone.getText()+ " : "+ produitsel);
+            HelloApplication.changeScene("pageGestionStock/editerProduitView",new ModifierProduitController(produitsel));
+        } else if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() ==1) {
+            TablePosition cell = produitTable.getSelectionModel().getSelectedCells().get(0);
+            int indexLigne = cell.getRow();
+            TableColumn colone = cell.getTableColumn();
+            FicheProduit produitsel = produitTable.getItems().get(indexLigne);
+            System.out.println("Simple-click ligne "+indexLigne+" colonne "+colone.getText()+ " : "+produitsel);
+            supprimerButton.setVisible(true);
+            int id = produitsel.getId();
+            supprimerButton.setOnAction(event1 -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation de suppression");
+                alert.setHeaderText("Êtes-vous sûr de vouloir supprimer la liste ?");
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.isPresent() && result.get() == ButtonType.OK) {
+                    //listeRepo.deleteListe(id,labelErreur);
+                    supprimerButton.setVisible(false);
+                }else {
+                    labelErreur.setText("Supression annulée");
+                    supprimerButton.setVisible(false);
+                }
+            });
+
+
+        }
     }
 }
