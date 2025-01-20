f8fd21f60732fdf4a804defd3e235ca814c4a3f6
Magne Rayan <r.magne@lprs.fr>
2025-01-20 14:04:38 +0100

    feat(AcceuilController):creation de la methode onListeSelection

--- src/main/java/com/example/projethsp/pageSecretaire/AcceuilController.java
+++ src/main/java/com/example/projethsp/pageSecretaire/AcceuilController.java
@@ -8,11 +8,13 @@ import com.example.projethsp.Repository.PatientRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -60,4 +62,40 @@ public class AcceuilController implements Initializable {
             System.out.println("Aucune session active à terminer.");
         }
     }
+
+    @FXML void onListeSelection(MouseEvent event) {
+        if (event.getButton() == MouseButton.PRIMARY) {
+            TablePosition cell = tableauPatient.getSelectionModel().getSelectedCells().get(0);
+                 int indexLigne = cell.getRow();
+                 Patient patientSelect = tableauPatient.getItems().get(indexLigne);
+
+            if (event.getClickCount() == 2) {
+                // Double clic pour édition
+                HelloApplication.changeScene("liste/EditerPatientView", new EditerPatientController(patientSelect));
+
+            } else if (event.getClickCount() == 1) {
+                // Simple clic pour suppression avec confirmation
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation de suppression");
+                //alert.setHeaderText("Suppression de la liste : " + nom);
+                alert.setContentText("Êtes-vous sûr de vouloir supprimer cette liste ?");
+
+                // Attente de la réponse de l'utilisateur
+                alert.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK) {
+                        // Suppression de la liste
+                        //ListeRepository listeRepository = new ListeRepository();
+                        try {
+                           // listeRepository.supprimerListe(id); // Suppression dans la base de données
+                        //} catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        //tableauListe.getItems().remove(indexLigne); // Suppression dans l'affichage
+                        //System.out.println("Liste supprimée : " + nom);
+                    }
+                });
+            }
+        }
+    }
+
 }
