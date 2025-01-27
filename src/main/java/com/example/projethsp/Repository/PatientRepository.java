919284abaf1f944c06c9fbacde6b3f1a8e3e7c63
Magne Rayan <r.magne@lprs.fr>
2025-01-27 14:39:57 +0100

    feat(PatientRepository):creation methode supprimerPatient.

--- src/main/java/com/example/projethsp/Repository/PatientRepository.java
+++ src/main/java/com/example/projethsp/Repository/PatientRepository.java
@@ -3,6 +3,7 @@ package com.example.projethsp.Repository;
 import com.example.projethsp.BDD.Bdd;
 import com.example.projethsp.Entity.Patient;
 import com.example.projethsp.Entity.Utilisateur;
+import com.example.projethsp.HelloApplication;
 import javafx.scene.control.Label;
 
 import java.sql.Connection;
@@ -23,7 +24,7 @@ public class PatientRepository {
             PreparedStatement requetePrepare = connection.prepareStatement(sql);
             ResultSet resultatRequette = requetePrepare.executeQuery();
             while (resultatRequette.next()) {
-                liste.add(new Patient(resultatRequette.getInt("id_utilisateur"),resultatRequette.getString("nom"),resultatRequette.getString("prenom"),resultatRequette.getString("email"),resultatRequette.getInt("ref_role"),resultatRequette.getString("telephone"),resultatRequette.getString("rue"),resultatRequette.getString("cp"),resultatRequette.getString("ville"),resultatRequette.getString("numSecu")));
+                liste.add(new Patient(resultatRequette.getInt("id_utilisateur"), resultatRequette.getString("nom"), resultatRequette.getString("prenom"), resultatRequette.getString("email"), resultatRequette.getInt("ref_role"), resultatRequette.getString("telephone"), resultatRequette.getString("rue"), resultatRequette.getString("cp"), resultatRequette.getString("ville"), resultatRequette.getString("numSecu")));
             }
 
 
@@ -34,21 +35,21 @@ public class PatientRepository {
         return liste;
     }
 
-    public Utilisateur getUserByEmail(String email){
+    public Utilisateur getUserByEmail(String email) {
         Bdd connexionBdd = new Bdd();
         String sql = "SELECT * FROM utilisateur WHERE email = ? ";
         try {
             PreparedStatement requete = connection.prepareStatement(sql);
-            requete.setString(1,email);
+            requete.setString(1, email);
             ResultSet resultatRequette = requete.executeQuery();
-            if(resultatRequette.next()){
+            if (resultatRequette.next()) {
                 int id = resultatRequette.getInt(1);
                 String nom = resultatRequette.getString(2);
                 String prenom = resultatRequette.getString(3);
                 String email1 = resultatRequette.getString(4);
                 String mdP = resultatRequette.getString(5);
                 int role = resultatRequette.getInt(6);
-                return new Utilisateur(id,nom, prenom,email1,mdP,role);
+                return new Utilisateur(id, nom, prenom, email1, mdP, role);
             }
 
         } catch (Exception e) {
@@ -57,24 +58,25 @@ public class PatientRepository {
         return null;
     }
 
-    public void inscription(String nom, String prenom, String email,String telephone, String rue, String cp, String ville , String numSecu, Label label){
-        if(this.getUserByEmail(email)!=null){
+    public void inscription(String nom, String prenom, String email, String telephone, String rue, String cp, String ville, String numSecu, Label label) {
+        if (this.getUserByEmail(email) != null) {
             label.setText("Erreur vous avez deja un compte");
-        }else {
+        } else {
             String sql1 = "INSERT INTO utilisateur (nom,prenom,email,telephone,rue,cp,ville,numSecu,ref_role) VALUES (?,?,?,?,?,?,?,?,7) ";
 
             try {
                 PreparedStatement requete = connection.prepareStatement(sql1);
-                requete.setString(1,nom);
-                requete.setString(2,prenom);
-                requete.setString(3,email);
-                requete.setString(4,telephone);
-                requete.setString(5,rue);
-                requete.setString(6,cp);
-                requete.setString(7,ville);
-                requete.setString(8,numSecu);
+                requete.setString(1, nom);
+                requete.setString(2, prenom);
+                requete.setString(3, email);
+                requete.setString(4, telephone);
+                requete.setString(5, rue);
+                requete.setString(6, cp);
+                requete.setString(7, ville);
+                requete.setString(8, numSecu);
                 requete.executeUpdate();
                 label.setText("Nouveaux patient enregistrer !");
+                HelloApplication.changeScene("pageSecretaire/acceuilView", "Acceuil");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -82,29 +84,42 @@ public class PatientRepository {
 
     }
 
-    public void editerPatient (String nom, String prenom, String email,String telephone, String rue, String cp, String ville , String numSecu, Label label) {
+    public void editerPatient(String nom, String prenom, String email, String telephone, String rue, String cp, String ville, String numSecu,int id, Label label) {
 
         Bdd bdd = new Bdd();
 
-        String sql1 = "UPDATE utilisateur SET nom = ?,prenom = ? ,email = ?,telephone = ?,rue = ?,cp = ?,ville = ?,numSecu = ?,ref_role = ? WHERE id_utilisateur = ?";
+        String sql1 = "UPDATE utilisateur SET nom = ?,prenom = ? ,email = ?,telephone = ?,rue = ?,cp = ?,ville = ?,numSecu = ? WHERE id_utilisateur = ?";
 
         try {
             PreparedStatement requete = connection.prepareStatement(sql1);
-            requete.setString(1,nom);
-            requete.setString(2,prenom);
-            requete.setString(3,email);
-            requete.setString(4,telephone);
-            requete.setString(5,rue);
-            requete.setString(6,cp);
-            requete.setString(7,ville);
-            requete.setString(8,numSecu);
+            requete.setString(1, nom);
+            requete.setString(2, prenom);
+            requete.setString(3, email);
+            requete.setString(4, telephone);
+            requete.setString(5, rue);
+            requete.setString(6, cp);
+            requete.setString(7, ville);
+            requete.setString(8, numSecu);
+            requete.setInt(9, id);
             requete.executeUpdate();
             label.setText("Patient modifié !");
+            HelloApplication.changeScene("pageSecretaire/acceuilView", "Acceuil");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
+    public void supprimerPatient(int id, Label label) {
 
+        String sql = "DELETE FROM utilisateur WHERE id_utilisateur = ?";
 
+        try {
+            PreparedStatement requete = connection.prepareStatement(sql);
+            requete.setInt(1, id);
+            requete.executeUpdate();
+            label.setText("L'utilisateur a bien été supprimer");
+        } catch (SQLException e) {
+            label.setText("erreur");
+            throw new RuntimeException(e);
+        }
+    }
 }
