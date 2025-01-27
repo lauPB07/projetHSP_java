ecf56032a551919302b67e704a1e1f77c217f3f4
Magne Rayan <r.magne@lprs.fr>
2025-01-27 14:36:39 +0100

    feat(AcceuilController):modification/ mise au propre methode onListeSelection.

--- src/main/java/com/example/projethsp/pageSecretaire/AcceuilController.java
+++ src/main/java/com/example/projethsp/pageSecretaire/AcceuilController.java
@@ -1,10 +1,12 @@
 package com.example.projethsp.pageSecretaire;
 
+import com.example.projethsp.Entity.FicheProduit;
 import com.example.projethsp.Entity.Patient;
 import com.example.projethsp.Entity.Utilisateur;
 import com.example.projethsp.Entity.Utilisateurconnecte;
 import com.example.projethsp.HelloApplication;
 import com.example.projethsp.Repository.PatientRepository;
+import com.example.projethsp.pageGestionStock.ModifierProduitController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +18,7 @@ import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AcceuilController implements Initializable {
@@ -23,11 +26,18 @@ public class AcceuilController implements Initializable {
     PatientRepository patientRepository = new PatientRepository();
 
     @FXML
+    private Button supprimer;
+
+    @FXML
+    private Label label;
+
+    @FXML
     private TableView<Patient> tableauPatient;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String [][] colonnes = {
-                {"ID User","id"},
+        String[][] colonnes = {
+                {"ID User", "id"},
                 {"Nom", "nom"},
                 {"Prenom", "prenom"},
                 {"Email", "email"},
@@ -39,22 +49,29 @@ public class AcceuilController implements Initializable {
                 {"Numéro securiter sociale", "numSecu"}
         };
 
-        for (int i = 0; i <colonnes.length; i++){
-            TableColumn<Patient,String> maColonne = new TableColumn<>(colonnes[i][0]);
+        for (int i = 0; i < colonnes.length; i++) {
+            TableColumn<Patient, String> maColonne = new TableColumn<>(colonnes[i][0]);
             maColonne.setCellValueFactory(new PropertyValueFactory<>(colonnes[i][1]));
             tableauPatient.getColumns().add(maColonne);
         }
         ArrayList<Patient> list = patientRepository.recupererPatient();
         tableauPatient.getItems().addAll(list);
+
+        supprimer.setVisible(false);
     }
 
     @FXML
-    public void inscption(ActionEvent event){
-        HelloApplication.changeScene("pageSecretaire/inscriptionView","Inscritpion Patient");
+    public void inscption(ActionEvent event) {
+        HelloApplication.changeScene("pageSecretaire/inscriptionView", "Inscritpion Patient");
     }
 
     @FXML
-    public void deconnexion(ActionEvent event){
+    void supprimer(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void deconnexion(ActionEvent event) {
         if (Utilisateurconnecte.clearInstance()) {
             System.out.println("Session terminée.");
             HelloApplication.changeScene("Acceuil/loginView", "Connexion");
@@ -63,39 +80,38 @@ public class AcceuilController implements Initializable {
         }
     }
 
-    @FXML void onListeSelection(MouseEvent event) {
-        if (event.getButton() == MouseButton.PRIMARY) {
+    @FXML
+    void onListeSelection(MouseEvent event) {
+        if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
             TablePosition cell = tableauPatient.getSelectionModel().getSelectedCells().get(0);
-                 int indexLigne = cell.getRow();
-                 Patient patientSelect = tableauPatient.getItems().get(indexLigne);
-
-            if (event.getClickCount() == 2) {
-                // Double clic pour édition
-                HelloApplication.changeScene("liste/EditerPatientView", new EditerPatientController(patientSelect));
-
-            } else if (event.getClickCount() == 1) {
-                // Simple clic pour suppression avec confirmation
+            int indexLigne = cell.getRow();
+            TableColumn colone = cell.getTableColumn();
+            Patient patient = tableauPatient.getItems().get(indexLigne);
+            System.out.println("Double-clique ligne " + indexLigne + " , colone  " + colone.getText() + " : " + patient);
+            HelloApplication.changeScene("pageSecretaire/editerPatientView", new EditerPatientController(patient));
+        } else if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
+            TablePosition cell = tableauPatient.getSelectionModel().getSelectedCells().get(0);
+            int indexLigne = cell.getRow();
+            TableColumn colone = cell.getTableColumn();
+            Patient patient = tableauPatient.getItems().get(indexLigne);
+            System.out.println("Simple-click ligne " + indexLigne + " colonne " + colone.getText() + " : " + patient);
+            supprimer.setVisible(true);
+            int id = patient.getId();
+            supprimer.setOnAction(event1 -> {
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("Confirmation de suppression");
-                //alert.setHeaderText("Suppression de la liste : " + nom);
-                alert.setContentText("Êtes-vous sûr de vouloir supprimer cette liste ?");
+                alert.setHeaderText("Êtes-vous sûr de vouloir supprimer la liste ?");
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.isPresent() && result.get() == ButtonType.OK) {
+                    patientRepository.supprimerPatient(id, label);
+                    supprimer.setVisible(false);
+                } else {
+                    label.setText("Supression annulée");
+                    supprimer.setVisible(false);
+                }
+            });
 
-                // Attente de la réponse de l'utilisateur
-                alert.showAndWait().ifPresent(response -> {
-                    if (response == ButtonType.OK) {
-                        // Suppression de la liste
-                        //ListeRepository listeRepository = new ListeRepository();
-                        try {
-                           // listeRepository.supprimerListe(id); // Suppression dans la base de données
-                        //} catch (SQLException e) {
-                            throw new RuntimeException(e);
-                        }
-                        //tableauListe.getItems().remove(indexLigne); // Suppression dans l'affichage
-                        //System.out.println("Liste supprimée : " + nom);
-                    }
-                });
-            }
+
         }
     }
-
 }
