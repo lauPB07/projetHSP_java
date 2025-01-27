9273ff88b291d15f61d9f2a8181e41f32483c841
djibr <d.korchi@lprs.fr>
2025-01-27 15:26:58 +0100

    Création entity demande et demandeProduit, création de leur répository, création page mes demande qui contient une lisste de toute les demande elle enméne vert la page produit demande controller qui contient la liste des produit de la demande

---
+++ src/main/java/com/example/projethsp/Repository/DemandeRepository.java
@@ -0,0 +1,37 @@
+package com.example.projethsp.Repository;
+
+import com.example.projethsp.BDD.Bdd;
+import com.example.projethsp.Entity.Demande;
+import com.example.projethsp.Entity.Utilisateur;
+import com.example.projethsp.Entity.Utilisateurconnecte;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class DemandeRepository {
+    static Bdd connexionBdd = new Bdd();
+    static Connection connection = connexionBdd.getBdd();
+
+    public ArrayList<Demande> select(){
+        ArrayList<Demande> liste = new ArrayList<>();
+        String sql = "SELECT * FROM demande WHERE ref_user = ?";
+        try {
+            PreparedStatement requetePrepare = connection.prepareStatement(sql);
+            requetePrepare.setInt(1, Utilisateurconnecte.getInstance().getId());
+            ResultSet resultatRequette = requetePrepare.executeQuery();
+            while (resultatRequette.next()) {
+                liste.add(new Demande(resultatRequette.getInt("id_utilisateur"),resultatRequette.getString("description"),resultatRequette.getString("ref_userDemande")));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+        return liste;
+    }
+}
