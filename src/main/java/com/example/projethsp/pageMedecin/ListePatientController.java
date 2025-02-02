2ee3596fdf553123b01153475903ac5ae7817191
djibr <d.korchi@lprs.fr>
2025-02-02 19:39:55 +0100

    créationt de lentity et du repository de la table fiche patient et création de la liste pour le medecin

--- src/main/java/com/example/projethsp/pageMedecin/ListePatientController.java
+++ src/main/java/com/example/projethsp/pageMedecin/ListePatientController.java
@@ -6,14 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class ListePatient implements Initializable {
+public class ListePatientController implements Initializable {
 
     private FichePatientRepository fichePatient=new FichePatientRepository();
 
@@ -53,6 +55,20 @@ public class ListePatient implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.hospitalisation.setVisible(false);
         this.ordonnance.setVisible(false);
+        String [][] colonnes = {
+                {"ID","id"},
+                {"Titre","titre"},
+                {"Description", "description"},
+                {"Valider","valider"}
+
+        };
+        for (int i = 0; i <colonnes.length; i++){
+            TableColumn<FichePatient,String> maColonne = new TableColumn<>(colonnes[i][0]);
+            maColonne.setCellValueFactory(new PropertyValueFactory<>(colonnes[i][1]));
+            liste.getColumns().add(maColonne);
+        }
         ArrayList<FichePatient> list = fichePatient.selectPatient();
+        liste.getItems().addAll(list);
+
     }
 }
--- src/main/java/com/example/projethsp/pageMedecin/MenuController.java
+++ src/main/java/com/example/projethsp/pageMedecin/MenuController.java
@@ -1,4 +1,37 @@
 package com.example.projethsp.pageMedecin;
 
+import com.example.projethsp.Entity.Utilisateurconnecte;
+import com.example.projethsp.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuController {
+
+    @FXML
+    private Button ajouter;
+
+    @FXML
+    private Button demande;
+
+    @FXML
+    private Button deconnexion;
+
+    @FXML
+    void onClickAjouter(ActionEvent event) {
+        HelloApplication.changeScene("pageMedecin/demandeStockView","Ajouter une demande");
+    }
+
+    @FXML
+    void onClickDemande(ActionEvent event) {
+        HelloApplication.changeScene("pageMedecin/mesDemandeView","Vos demandes");
+    }
+
+    public void onClickDeconnexion(ActionEvent actionEvent) {
+        if (Utilisateurconnecte.clearInstance()) {
+            HelloApplication.changeScene("acceuil/loginView", "Connexion");
+        } else {
+            System.out.println("Aucune session active à fermer.");
+        }
+    }
 }
